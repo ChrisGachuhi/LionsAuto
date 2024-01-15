804a9b81806780c78d23d5076dc2ebdc28782bfa
GRuoro <chrisgachuhi@gmail.com>
2024-01-15 04:35:39 +0300

    Update with Form component layout

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="Section Footer">Footer</div>
   )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,6 +1,24 @@
-
 export const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div className="Section Form">
+      <div className="contact-center">
+        <span>CONTACT US</span>
+        <span>Don&#39;t Hesitate to</span>
+        <span>Contact us for any</span>
+        <span>Information</span>
+
+        <p>Call us for immediate support on this number</p>
+        <span>+254 712345678</span>
+      </div>
+
+      <form className="form-submit">
+        <input type="text" placeholder="Your Name*" required />
+        <input type="email" placeholder="Email Address*" required />
+        <input type="text" placeholder="Phone number" required />
+        <input type="text" placeholder="Product Interested" required />
+        <textarea name="" id="" placeholder="Your Message"></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
+};
