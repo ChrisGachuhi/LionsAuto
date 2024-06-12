fbaf3a8546898f192b12b56f9c28cd95f45b6bec
ChrisGachuhi <chrisgachuhi@gmail.com>
2024-06-12 08:00:05 +0300

    Contact page formating

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.872136492844!2d36.869619675035906!3d-1.2478357355854177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f142b4a91433d%3A0x9de585f7b9fd8abb!2sGym%20Plaza!5e0!3m2!1sen!2ske!4v1718165608640!5m2!1sen!2ske"
           width="100%"
           height="90%"
-          style={{ border: 0 }}
+          style={{ border: 0 , borderRadius: 10}}
           allowFullScreen=""
           loading="lazy"
         ></iframe>
